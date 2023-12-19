a5e21facf1ac4cc4bc8857921d789828e8b02bb4
oceandie <diego.bruciaferri@metoffice.gov.uk>
2023-12-19 13:52:29 +0000

    cleaning and generalising

--- src/forcing/atm/create_wgts_for_interp_on_the_fly.sh
+++ src/forcing/atm/create_wgts_for_interp_on_the_fly.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 cmd="/data/users/dbruciaf/NEMO/v_4.2/nemo/tools/WEIGHTS/scripshape.exe"
-nam_bil="namelist_bilin"
-nam_bic="namelist_bicub"
+nam_bil="namelist_bilin_gs08"
+nam_bic="namelist_bicub_gs08"
 
 source /data/users/dbruciaf/NEMO/ukmo_utils/use_vdi.sh
 
