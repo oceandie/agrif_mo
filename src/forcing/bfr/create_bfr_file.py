4035d23f8264ef631c22b1508a3fddbad92d0709
oceandie <diego.bruciaferri@metoffice.gov.uk>
2024-01-10 14:29:08 +0000

    adding orca08 case

--- src/forcing/bfr/create_bfr_file.py
+++ src/forcing/bfr/create_bfr_file.py
@@ -2,14 +2,14 @@
 
 import xarray as xr
 
-mesh = '/data/users/dbruciaf/AGRIF/gs/orca04/1_mesh_mask_large.nc'
+mesh = '/data/users/dbruciaf/AGRIF-NAtl/gs/orca08/1_mesh_mask.nc'
 
 ds_msh = xr.open_dataset(mesh)
 ds_bfr = ds_msh[['nav_lon','nav_lat','time_counter','glamt']]
 ds_bfr = ds_bfr.rename({'glamt':'bfr_coef'})
 ds_bfr.bfr_coef[:,:,:] = 0. 
 
-outdir = '/data/users/dbruciaf/AGRIF/gs/orca04/bfr/'
-outfile = 'bfr_coef_gs-orca04.nc'
+outdir = '/data/users/dbruciaf/AGRIF-NAtl/gs/orca08/bfr/'
+outfile = 'bfr_coef_gs-orca08.nc'
 ds_bfr.to_netcdf(outdir+outfile)
 
